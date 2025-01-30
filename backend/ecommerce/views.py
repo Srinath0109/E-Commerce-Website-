51d998d6d608efc2095d1074030b69577a46a80a
Srinath Murugesan <162076816+Srinath0109@users.noreply.github.com>
2025-01-30 21:05:59 +0530

    Create views.py

---
+++ backend/ecommerce/views.py
@@ -0,0 +1,11 @@
+from rest_framework import viewsets
+from .models import Product, Cart
+from .serializers import ProductSerializer, CartSerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class CartViewSet(viewsets.ModelViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
